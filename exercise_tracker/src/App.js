54417cde1ff2e88297f6b5fd26e7136adb054e43
jll5677 <jianli@Jians-MacBook-Air.local>
2019-11-19 23:24:32 -0500

    created react app folder and installed bootstrap

---
+++ exercise_tracker/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div classname="container">
+        <h2>Exercise Tracker</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
