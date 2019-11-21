8c99a75e32ec755c35f6a3db7774fb2d8353cc10
jll5677 <jianli@Jians-MacBook-Air.local>
2019-11-20 22:58:27 -0500

    Adding nav bar

---
+++ exercise_tracker/src/components/create.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Create extends Component {
+    render() {
+        return (
+            <div>
+                <p>Please log your exercise</p>
+            </div>
+        )
+    }
+}
