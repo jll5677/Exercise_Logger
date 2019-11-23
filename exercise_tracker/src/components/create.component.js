63d2e25efbbd9bbeee05776f007876e264f692d4
jll5677 <jianli@Jians-MacBook-Air.local>
2019-11-22 21:24:09 -0500

    Added form submittion logic

--- exercise_tracker/src/components/create.component.js
+++ exercise_tracker/src/components/create.component.js
@@ -1,6 +1,57 @@
 import React, { Component } from 'react';
 
 export default class Create extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeExerciseName = this.onChangeExerciseName.bind(this);
+        this.onChangeExerciseWeight = this.onChangeExerciseName.bind(this);
+        this.onChangeExerciseSet = this.onChangeExerciseSet.bind(this);
+        this.onChangeExerciseRep = this.onChangeExerciseRep.bind(this);
+
+        this.state = {
+            Exercise_name: '',
+            Exercise_weight: '',
+            Exercise_set: '',
+            Exercise_rep: ''
+        }
+    }
+
+    onChangeExerciseName(e) {
+        this.setState({
+            Exercise_name: e.target.value
+        });
+    }
+
+    onChangeExerciseWeight(e) {
+        this.setState({
+            Exercise_weight: e.target.value
+        });
+    }
+
+    onChangeExerciseSet(e) {
+        this.setState({
+            Exercise_set: e.target.value
+        });
+    }
+
+    onChangeExerciseRep(e) {
+        this.setState({
+            Exercise_rep: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log('The values are ${this.state.Exercise_name}, ${this.state.Exercise_weight}, ${this.state.Exercise_set}, ${this.state.Exercise_rep}');
+        this.setState({
+            Exercise_name: '',
+            Exercise_weight: '',
+            Exercise_set: '',
+            Exercise_rep: ''
+        });
+    }
+
+
     render() {
         return (
             <div style ={{marginTop: 10}}>
@@ -8,63 +59,29 @@ export default class Create extends Component {
                 <form>
                     <div className="form-group">
                         <label>Exercise 1 Name:</label>
-                        <input type="text"/>
+                        <input 
+                            type="text"
+                            value={this.state.Exercise_name}
+                            onChange={this.onChangeExerciseName}
+                        />
                         <label>Exercise 1 Weight (lbs):</label>
-                        <input type="number"/>
+                        <input 
+                            type="number"
+                            value={this.state.Exercise_weight}
+                            onChange={this.onChangeExerciseWeight}
+                        />
                         <label>Exercise 1 Sets:</label>
-                        <input type="number"/>
+                        <input 
+                            type="number"
+                            value={this.state.Exercise_set}
+                            onChange={this.onChangeExerciseSet}
+                        />
                         <label>Exercise 1 Reps:</label>
-                        <input type="number"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Exercise 2 Name:</label>
-                        <input type="text"/>
-                        <label>Exercise 2 Weight (lbs):</label>
-                        <input type="number"/>
-                        <label>Exercise 2 Sets:</label>
-                        <input type="number"/>
-                        <label>Exercise 2 Reps:</label>
-                        <input type="number"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Exercise 3 Name:</label>
-                        <input type="text"/>
-                        <label>Exercise 3 Weight (lbs):</label>
-                        <input type="number"/>
-                        <label>Exercise 3 Sets:</label>
-                        <input type="number"/>
-                        <label>Exercise 3 Reps:</label>
-                        <input type="number"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Exercise 4 Name:</label>
-                        <input type="text"/>
-                        <label>Exercise 4 Weight (lbs):</label>
-                        <input type="number"/>
-                        <label>Exercise 4 Sets:</label>
-                        <input type="number"/>
-                        <label>Exercise 4 Reps:</label>
-                        <input type="number"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Exercise 5 Name:</label>
-                        <input type="text"/>
-                        <label>Exercise 5 Weight (lbs):</label>
-                        <input type="number"/>
-                        <label>Exercise 5 Sets:</label>
-                        <input type="number"/>
-                        <label>Exercise 5 Reps:</label>
-                        <input type="number"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Exercise 6 Name:</label>
-                        <input type="text"/>
-                        <label>Exercise 6 Weight (lbs):</label>
-                        <input type="number"/>
-                        <label>Exercise 6 Sets:</label>
-                        <input type="number"/>
-                        <label>Exercise 6 Reps:</label>
-                        <input type="number"/>
+                        <input 
+                            type="number"
+                            value={this.state.onChangeExerciseRep}
+                            onChange={this.onChangeExerciseRep}
+                        />
                     </div>
                     <div className="form-group">
                         <input type="submit" value="Submit" className="btn btn-primary"/>
