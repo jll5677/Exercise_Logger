9b6f117112519c823c031289973343ec88525993
jll5677 <jianli@Jians-Air.fios-router.home>
2019-11-29 10:03:41 -0500

    added index page

---
+++ exercise_tracker/src/components/TableRow.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class TableRow extends Component {
+    render() {
+        return (
+            <tr>
+                <td>
+                    {this.props.obj.exercise_name}
+                </td>
+                <td>
+                    {this.props.obj.exercise_weight}
+                </td>
+                <td>
+                    {this.props.obj.exercise_set}
+                </td>
+                <td>
+                    {this.props.obj.exercise_rep}
+                </td>
+                <td>
+                    <button className="btn btn-primary">Edit</button>
+                </td>
+                <td>
+                    <button className="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default TableRow;
--- exercise_tracker/src/components/index.component.js
+++ exercise_tracker/src/components/index.component.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
 
 export default class Index extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {exercise: []};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:4000/exercise')
+            .then(response => {
+                this.setState({ exercise: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    tabRow(){
+        return this.state.exercise.map(function(object,i){
+            return <TableRow obj={object} key={i} />;
+        });
+    }
+
     render() {
         return (
             <div>
-                <p>Welcome to your Exercise Tracker</p>
+                <h3 align="center">Exercise List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thread>
+                        <tr>
+                            <th>Name</th>
+                            <th>Weight</th>
+                            <th>Sets</th>
+                            <th>Reps</th>
+                            <th colSpan ="2">Action</th>
+                        </tr>
+                    </thread>
+                    <tbody>
+                        { this.tabRow() }
+                    </tbody>
+                </table>
             </div>
-        )
+        );
     }
 }
