8ef048060e7e8b750ccb200d72ef1f5ccb09d286
jll5677 <jianli@Jians-MacBook-Air.local>
2019-12-03 23:17:35 -0500

    Added delete functionality

--- exercise_tracker/src/components/TableRow.js
+++ exercise_tracker/src/components/TableRow.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class TableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+
+    delete() {
+        axios.get('http://localhost:4000/exercise/delete/'+this.props.obj._id)
+            .then(console.log('Deleted'))
+            .catch(err => console.log(err))
+    }
+
     render() {
         return (
             <tr>
@@ -21,7 +33,7 @@ class TableRow extends Component {
                     <Link to={"/edit/"+this.props.obj._id} className="btn btn-primary">Edit</Link>
                 </td>
                 <td>
-                    <button className="btn btn-danger">Delete</button>
+                    <button onClick={this.delete} className="btn btn-danger">Delete</button>
                 </td>
             </tr>
         );
